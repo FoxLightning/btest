92194f57519cf5af6fdb0019b3f3f3fecc4175cb
FoxLightning <bogdanlisichenko@gmail.com>
2026-02-11 09:26:04 +0100

    Create / delete profiling

--- BTest/ECSCore/Test/ObjectCreateDeleteTest.cpp
+++ BTest/ECSCore/Test/ObjectCreateDeleteTest.cpp
@@ -5,16 +5,20 @@
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <ratio>
+#include <print>
 
 #include "ECSCore/Entity.hpp"
 #include "ECSCore/Object.hpp"
 #include "ECSCore/ObjectManager.hpp"
 
+// TODO move profiling to different test
 int main()
 {
-    const auto startTime = std::chrono::steady_clock::now();
-    const auto entity     = std::make_shared<ECSCore::Entity>();
-    const auto managerPtr = entity->CreateManager<ECSCore::TObjectManager<ECSCore::Object>>();
+    const auto startTime       = std::chrono::steady_clock::now();
+    auto       endCreationTime = startTime;
+    const auto entity          = std::make_shared<ECSCore::Entity>();
+    const auto managerPtr      = entity->CreateManager<ECSCore::TObjectManager<ECSCore::Object>>();
 
     constexpr int32_t targetObjectCount{1000};
     std::println(std::cout, R"(Creating {} objects.)", targetObjectCount);
@@ -31,16 +35,18 @@ int main()
                          managerPtr.lock()->GetObjectCount());
             return 1;
         }
+        std::println(std::cout, R"(Deleting all objects.)", targetObjectCount);
+        endCreationTime = std::chrono::steady_clock::now();
     }
-    std::println(std::cout, R"(Deleting all objects.)", targetObjectCount);
     if (managerPtr.lock()->GetObjectCount() != 0)
     {
         std::println(std::cerr, R"(Manager should have no registered objects, but have {}.)", targetObjectCount,
                      managerPtr.lock()->GetObjectCount());
         return 1;
     }
-    const auto endTime = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration<double, std::milli>(endTime - startTime).count();
-    println(std::cout, R"(Test successfully finished. Execution time {:.6} ms)", duration);
+    const auto endDeleteTime  = std::chrono::steady_clock::now();
+    auto       createDuration = std::chrono::duration<double, std::milli>(endCreationTime - startTime).count();
+    auto       deleteDuration = std::chrono::duration<double, std::milli>(endDeleteTime - endCreationTime).count();
+    println(std::cout, "Test successfully finished.\nCreate time {:.6} ms.\nDelete time {:.6} ms", createDuration, deleteDuration);
     return 0;
 }
