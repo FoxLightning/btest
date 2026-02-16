fe8a00c9d447e1155d525f218c902f96faff4a61
FoxLightning <bogdanlisichenko@gmail.com>
2026-02-16 08:30:56 +0100

    Container abstraction

--- BTest/ECSCore/Include/ECSCore/ObjectManager.hpp
+++ BTest/ECSCore/Include/ECSCore/ObjectManager.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <algorithm>
+#include <concepts>
 #include <cstddef>
 #include <cstdint>
 #include <functional>
@@ -17,15 +18,70 @@ namespace ECSCore
 class Entity;
 class Object;
 
+namespace FunLib
+{
+
+template<typename tElement>
+class IObjectContainer
+{
+  public:
+    virtual ~IObjectContainer() = default;
+
+    virtual void                          Add(std::weak_ptr<tElement> object)                                       = 0;
+    virtual void                          EraseExpired()                                                            = 0;
+    virtual std::weak_ptr<tElement>       Find(const std::function<bool(std::weak_ptr<tElement>)>& predicate)       = 0;
+    virtual std::weak_ptr<const tElement> Find(const std::function<bool(std::weak_ptr<tElement>)>& predicate) const = 0;
+    virtual void                 ForEach(const std::function<void(std::weak_ptr<tElement>)>& function) const        = 0;
+    [[nodiscard]] virtual size_t Size() const                                                                       = 0;
+};
+
+class TDynamicArrayContainer : public IObjectContainer<Object>
+{
+  public:
+    void Add(std::weak_ptr<Object> object) override
+    {
+        objectRegistry.emplace_back(object);
+    }
+
+    void EraseExpired() override
+    {
+        std::erase_if(objectRegistry, [](const std::weak_ptr<Object>& weakObject) { return weakObject.expired(); });
+    }
+    std::weak_ptr<Object> Find(const std::function<bool(std::weak_ptr<Object>)>& predicate) override
+    {
+        const auto iterator = std::ranges::find_if(objectRegistry, predicate);
+        return iterator != objectRegistry.end() ? std::weak_ptr<Object>{*iterator} : std::weak_ptr<Object>{};
+    }
+    std::weak_ptr<const Object> Find(const std::function<bool(std::weak_ptr<Object>)>& predicate) const override
+    {
+        const auto iterator = std::ranges::find_if(objectRegistry, predicate);
+        return iterator != objectRegistry.end() ? std::weak_ptr<const Object>{*iterator}
+                                                : std::weak_ptr<const Object>{};
+    }
+    void ForEach(const std::function<void(std::weak_ptr<Object>)>& function) const override
+    {
+        std::ranges::for_each(objectRegistry, function);
+    }
+    [[nodiscard]] size_t Size() const override
+    {
+        return objectRegistry.size();
+    }
+
+  private:
+    std::vector<std::weak_ptr<Object>> objectRegistry;
+};
+
+} // namespace FunLib
+
 class IObjectManager
 {
   public:
-    virtual ~IObjectManager() = default;
+    virtual ~IObjectManager()                                             = default;
     virtual bool TryAttachObjectToComponent(std::weak_ptr<Object> object) = 0;
     virtual void EraseExpiredWeakPointers()                               = 0;
 };
 
-template<typename tObjectType>
+template<typename tObjectType, typename tObjectContainer = FunLib::TDynamicArrayContainer>
 class TObjectManager : public IObjectManager
 {
   public:
@@ -50,46 +106,46 @@ class TObjectManager : public IObjectManager
 
     [[nodiscard]] size_t GetObjectCount() const
     {
-        return objectRegistry.size() - expiredCount;
+        return objectRegistry.Size() - expiredCount;
     }
 
   private:
-    static constexpr int32_t GExpiredLimit{ 100 };
-    int32_t expiredCount{ 0 };
-    std::vector<std::weak_ptr<Object>> objectRegistry;
+    static constexpr int32_t GExpiredLimit{100};
+    int32_t                  expiredCount{0};
+    tObjectContainer         objectRegistry;
 };
 
-template<typename tObjectType>
-bool TObjectManager<tObjectType>::PostAttachObjectToComponent(std::weak_ptr<Object> /* object */)
+template<typename tObjectType, typename tObjectContainer>
+bool TObjectManager<tObjectType, tObjectContainer>::PostAttachObjectToComponent(std::weak_ptr<Object> /* object */)
 {
     return false;
 }
 
-template<typename tObjectType>
-bool TObjectManager<tObjectType>::TryAttachObjectToComponent(std::weak_ptr<Object> object)
+template<typename tObjectType, typename tObjectContainer>
+bool TObjectManager<tObjectType, tObjectContainer>::TryAttachObjectToComponent(std::weak_ptr<Object> object)
 {
     if (std::dynamic_pointer_cast<tObjectType>(object.lock()))
     {
-        objectRegistry.emplace_back(object);
+        objectRegistry.Add(object);
         return PostAttachObjectToComponent(object);
     }
     return false;
 }
 
-template<typename tObjectType>
-void TObjectManager<tObjectType>::EraseExpiredWeakPointers()
+template<typename tObjectType, typename tObjectContainer>
+void TObjectManager<tObjectType, tObjectContainer>::EraseExpiredWeakPointers()
 {
     if (expiredCount > GExpiredLimit)
     {
-        std::erase_if(objectRegistry, [](const std::weak_ptr<Object>& weakObject) { return weakObject.expired(); });
+        objectRegistry.EraseExpired();
         expiredCount = 0;
         return;
     }
     ++expiredCount;
 }
 
-template<typename tObjectType>
-std::weak_ptr<Object> TObjectManager<tObjectType>::GetObject(
+template<typename tObjectType, typename tObjectContainer>
+std::weak_ptr<Object> TObjectManager<tObjectType, tObjectContainer>::GetObject(
     const std::function<bool(std::weak_ptr<Object>)>& predicate)
 {
     const std::weak_ptr constWeak = std::as_const(*this).GetObject(predicate);
@@ -102,18 +158,18 @@ std::weak_ptr<Object> TObjectManager<tObjectType>::GetObject(
     return {};
 }
 
-template<typename tObjectType>
-std::weak_ptr<const Object> TObjectManager<tObjectType>::GetObject(
+template<typename tObjectType, typename tObjectContainer>
+std::weak_ptr<const Object> TObjectManager<tObjectType, tObjectContainer>::GetObject(
     const std::function<bool(std::weak_ptr<Object>)>& predicate) const
 {
-    const auto iterator = std::ranges::find_if(objectRegistry, predicate);
-    return iterator != objectRegistry.end() ? std::weak_ptr<const Object>{*iterator} : std::weak_ptr<const Object>{};
+    return objectRegistry.Find(predicate);
 }
 
-template<typename tObjectType>
-void TObjectManager<tObjectType>::ForEachObject(const std::function<void(std::weak_ptr<Object>)>& function) const
+template<typename tObjectType, typename tObjectContainer>
+void TObjectManager<tObjectType, tObjectContainer>::ForEachObject(
+    const std::function<void(std::weak_ptr<Object>)>& function) const
 {
-    std::ranges::for_each(objectRegistry, function);
+    objectRegistry.ForEach(function);
 }
 
 } // namespace ECSCore
